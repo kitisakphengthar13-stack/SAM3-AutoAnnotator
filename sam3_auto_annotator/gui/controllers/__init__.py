9af47d0c421bcd72ad858cda12540e5d0943fb0f
Fern Fern <kitisakphengthar13@gmail.com>
2026-09-05 17:10:04 +0700

    refactor: expose workstation mode with active controller

--- sam3_auto_annotator/gui/controllers/__init__.py
+++ sam3_auto_annotator/gui/controllers/__init__.py
@@ -1,5 +1,6 @@
 """Use-case controllers for the canvas workstation."""
 
+from sam3_auto_annotator.gui.controllers.state import UiMode
 from sam3_auto_annotator.gui.controllers.workstation_controller import WorkstationController
 
-__all__ = ["WorkstationController"]
+__all__ = ["UiMode", "WorkstationController"]
