f5ff8ce2f43dbb565715f7988277c623041a2152
Fern Fern <kitisakphengthar13@gmail.com>
2026-09-05 16:48:12 +0700

    refactor: delete active-class compatibility bridge

--- sam3_auto_annotator/gui/coordinators/annotation_interaction.py
+++ sam3_auto_annotator/gui/coordinators/annotation_interaction.py
@@ -8,35 +8,9 @@ class AnnotationInteractionCoordinator:
 
     def __init__(self, window):
         self.window = window
-        self._draw_class_restore = None
-        window.canvas.box_drawn.connect(self._prepare_active_class_for_draw)
         window.actions.mark_reviewed.triggered.connect(
             lambda _checked=False: QTimer.singleShot(0, self._advance_after_review)
         )
-
-    def _prepare_active_class_for_draw(self, _box):
-        """Bridge legacy controller lookup until active class is controller-owned."""
-        controller = self.window.controller
-        image = controller.current_image if controller is not None else None
-        if image is None:
-            return
-        previous_index = self.window.annotation.class_combo.currentIndex()
-        previous_count = len(image.annotations)
-        self.window.annotation.class_combo.setCurrentIndex(
-            self.window.canvas_area.active_class_combo.currentIndex()
-        )
-        self._draw_class_restore = (image, previous_count, previous_index)
-        QTimer.singleShot(0, self._restore_draw_class_if_failed)
-
-    def _restore_draw_class_if_failed(self):
-        restore = self._draw_class_restore
-        self._draw_class_restore = None
-        controller = self.window.controller
-        if restore is None or controller is None:
-            return
-        image, previous_count, previous_index = restore
-        if controller.current_image is image and len(image.annotations) == previous_count:
-            self.window.annotation.class_combo.setCurrentIndex(previous_index)
 
     def _advance_after_review(self):
         controller = self.window.controller
