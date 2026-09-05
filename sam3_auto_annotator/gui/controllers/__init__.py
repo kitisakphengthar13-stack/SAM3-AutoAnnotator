cc5157bd8c2fa03e800ce47b10b835806fd4f6a9
Fern Fern <kitisakphengthar13@gmail.com>
2026-09-05 16:45:51 +0700

    refactor: introduce active application controller package

---
+++ sam3_auto_annotator/gui/controllers/__init__.py
@@ -0,0 +1,5 @@
+"""Use-case controllers for the canvas workstation."""
+
+from sam3_auto_annotator.gui.controllers.workstation_controller import WorkstationController
+
+__all__ = ["WorkstationController"]
