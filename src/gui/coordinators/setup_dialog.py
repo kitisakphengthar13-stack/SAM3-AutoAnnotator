0799d1fba4c5191a1d8d85582b433c4a974ed3e6
Fern Fern <kitisakphengthar13@gmail.com>
2026-09-05 18:45:05 +0700

    refactor: remove obsolete setup snapshot timer shim

--- src/gui/coordinators/setup_dialog.py
+++ src/gui/coordinators/setup_dialog.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from PySide6.QtCore import QTimer
-
 from services.project_service import parse_prompts
 
 
@@ -11,7 +9,6 @@ class SetupDialogCoordinator:
     def __init__(self, window):
         self.window = window
         self._snapshot = None
-        self._snapshot_pending = False
         window.setup.apply_requested.connect(self.apply)
         window.setup.cancel_requested.connect(window.setup_dialog.reject)
         window.setup_dialog.rejected.connect(self.restore)
@@ -19,17 +16,10 @@ class SetupDialogCoordinator:
     def show(self):
         dialog = self.window.setup_dialog
         if not dialog.isVisible():
-            self._snapshot_pending = True
-            QTimer.singleShot(0, self._capture)
+            self._snapshot = self.window.setup.snapshot()
         dialog.show()
         dialog.raise_()
         dialog.activateWindow()
-
-    def _capture(self):
-        if not self._snapshot_pending or not self.window.setup_dialog.isVisible():
-            return
-        self._snapshot_pending = False
-        self._snapshot = self.window.setup.snapshot()
 
     def apply(self):
         controller = self.window.controller
@@ -47,14 +37,12 @@ class SetupDialogCoordinator:
             return
 
         self._snapshot = None
-        self._snapshot_pending = False
         self.window.setup_dialog.accept()
 
     def restore(self):
         if self._snapshot is not None:
             self.window.setup.restore_snapshot(self._snapshot)
         self._snapshot = None
-        self._snapshot_pending = False
         controller = self.window.controller
         if controller is not None:
             controller.presentation.update_actions()
