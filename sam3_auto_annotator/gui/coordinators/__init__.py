e0f0afe4d723b818011f1324dbaef69ae0ff4a4a
Fern Fern <kitisakphengthar13@gmail.com>
2026-09-05 16:44:06 +0700

    refactor: export annotation interaction coordinator

--- sam3_auto_annotator/gui/coordinators/__init__.py
+++ sam3_auto_annotator/gui/coordinators/__init__.py
@@ -1,11 +1,13 @@
 """GUI workflow coordinators that keep QMainWindow composition-focused."""
 
 from sam3_auto_annotator.gui.coordinators.annotation_history import AnnotationHistoryCoordinator
+from sam3_auto_annotator.gui.coordinators.annotation_interaction import AnnotationInteractionCoordinator
 from sam3_auto_annotator.gui.coordinators.export_dialog import ExportDialogCoordinator
 from sam3_auto_annotator.gui.coordinators.setup_dialog import SetupDialogCoordinator
 
 __all__ = [
     "AnnotationHistoryCoordinator",
+    "AnnotationInteractionCoordinator",
     "ExportDialogCoordinator",
     "SetupDialogCoordinator",
 ]
